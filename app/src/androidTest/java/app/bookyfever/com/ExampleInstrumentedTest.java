7ec447c8124c073e0d79c8277e47d5811960d933
Onkar Nene <Onkarn92@users.noreply.github.com>
2020-10-30 21:23:58 +0530

    Migrate project to AndroidX | Added Kotlin support and required dependencies.

--- app/src/androidTest/java/app/bookyfever/com/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/app/bookyfever/com/ExampleInstrumentedTest.java
@@ -1,13 +1,14 @@
 package app.bookyfever.com;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.*;
+import org.junit.runner.*;
 
 import static org.junit.Assert.*;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,11 +17,13 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("app.bookyfever.com", appContext.getPackageName());
-    }
+	
+	@Test
+	public void useAppContext() throws Exception {
+		
+		// Context of the app under test.
+		Context appContext = InstrumentationRegistry.getInstrumentation()
+		                                            .getTargetContext();
+		assertEquals("app.bookyfever.com", appContext.getPackageName());
+	}
 }
